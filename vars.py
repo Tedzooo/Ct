c8eb989f40f821619dd053141c4d289829f3a07c
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-07-25 18:23:45 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # For Bot
-BOT_NAME = os.environ.get("BOT_NAME", "Tools-Bot")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_NAME = os.environ.get("TEDZO", "Tools-Bot")
+BOT_TOKEN = os.environ.get("7384862816:AAHvxhtB-4rroiSlZmbgf0MUfKP25hNpmeA")
+API_ID = int(os.environ.get("15453419"))
+API_HASH = os.environ.get("6c9c9e5a2e65daf192e7dd9dde026f45")
 
 # Authorisation and Administrators
 AUTH = bool(os.environ.get("AUTH", True))
-ADMINS = set(int(x) for x in os.environ.get("ADMINS", "").split())
+ADMINS = set(int(x) for x in os.environ.get("6778786531", "").split())
 
 # Database (MongoDB)
-DATABASE_URL = os.environ.get("DATABASE_URL")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Tools-Bot")
+DATABASE_URL = os.environ.get("mongodb+srv://misnaminnu97:misnaminnu97@cluster0.t6cgltc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "test")
