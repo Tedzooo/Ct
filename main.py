6fdb4a94ab752dd76f11c24d5d4e800806df3a8d
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-07-25 19:08:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ API_ID = '15453419'
 API_HASH = '6c9c9e5a2e65daf192e7dd9dde026f45'
 BOT_TOKEN = '7384862816:AAHvxhtB-4rroiSlZmbgf0MUfKP25hNpmeA'
 # Def
-app = Client(
+bot = Client(
     "tedzo",
     bot_token=BOT_TOKEN,
     api_id=API_ID,
