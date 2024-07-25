d47faf1071c3d215b13a2484f9ce5c85c795f787
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-07-25 19:34:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import vars
 from pyrogram import Client
+from dotenv import load_dotenv
 
 
 API_ID = '15453419'
 API_HASH = '6c9c9e5a2e65daf192e7dd9dde026f45'
 BOT_TOKEN = '7384862816:AAHvxhtB-4rroiSlZmbgf0MUfKP25hNpmeA'
 # Def
-pyro = Client(
+
+load_dotenv()
+
+app = Client(
     "tedzo",
     bot_token=BOT_TOKEN,
     api_id=API_ID,
@@ -14,4 +18,11 @@ pyro = Client(
     workers=100,
     plugins=dict(root="bot")
 )
-pyro.run()
+
+
+
+
+
+
+#myr
+app.run()
