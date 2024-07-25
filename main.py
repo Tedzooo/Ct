fdec25415100e55112e16f2bd61faf491a6b4d1e
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-07-25 19:02:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import vars
 from pyrogram import Client
-from vars import BOT_TOKEN,API_ID,API_HASH
 
+
+API_ID = '15453419'
+API_HASH = '6c9c9e5a2e65daf192e7dd9dde026f45'
+BOT_TOKEN = '7384862816:AAHvxhtB-4rroiSlZmbgf0MUfKP25hNpmeA'
+# Def
 app = Client(
     "tedzo",
     bot_token=BOT_TOKEN,
